b6e7ead1e27a59b1ca6a5e625300d342b97104f9
Izzy Brand <izzy@Izzys-MacBook-Pro.local>
2017-04-04 18:40:32 -0400

    Pulled some current_action flow out of step in droneberry. Switched away from error checking mission upload. Added shutdown command

--- droneberry/droneberry.py
+++ droneberry/droneberry.py
@@ -24,20 +24,23 @@ import datetime
 import time
 # from gripper import Gripper
 # from gpiozero import Button
-from missionHandler import upload_and_verify
+from missionHandler import upload
 from serverberry import ServerInterface
 from dronekit import connect, VehicleMode
 from pymavlink import mavutil
 import sys
+import os
 
 
 
 class Drone:
 	_COMMAND_SET_MISSION 	= 'updatezone'
+	_COMMAND_START			= 'start'
 	_COMMAND_TAKEOFF 		= 'takeoff'
 	_COMMAND_LAND 			= 'land'
 	_COMMAND_PAUSE			= 'pause'
 	_COMMAND_RTL 			= 'rtl'
+	_COMMAND_SHUTDOWN		= 'shutdown'
 
 	def start(self):
 		self.server = ServerInterface()
@@ -45,7 +48,8 @@ class Drone:
 		# self.pixhawk = connect('/dev/ttyS0', baud = 57600, wait_ready=True) # for on the raspberry PI via telem2
 		self._log('Connected to pixhawk.')
 		self._prev_command = ''
-		self.current_action = ''
+		self._arming_window_start = 0
+		self.current_action = 'idle'
 		config_loaded = self._load_config() # load info about the uid and auth
 		online = True # TODO: verify internet connection
 		# self.gripper = Gripper(18) # set up the gripper
@@ -77,50 +81,53 @@ class Drone:
 
 		# ACT ON NEW COMMAND COMMAND
 		if received_command != self._prev_command:
-			self._log('NEW COMMAND - ' + received_command)
+			self._log('received command - ' + received_command)
 
-			if received_command == self._COMMAND_RTL:
-				if self.current_action == 'arm' or self.current_action == 'takeoff':
-					# disarm if we haven't taken off yet
-					self.current_action = 'disarm'
-				else: self.current_action = 'rtl'
-
-			elif received_command == self._COMMAND_PAUSE:
-				if self.current_action == 'arm' or self.current_action == 'takeoff':
-					# disarm if we haven't taken off yet
-					self.current_action = 'disarm'
-				else: self.current_action = 'pause'
-
+			if received_command == self._COMMAND_START:
+				self.set_action('prearm')
 			elif received_command == self._COMMAND_TAKEOFF:
-				if self.current_action == 'wait_arm': self.current_action = 'arm'
-				else: self._log('WARNING - Cannot arm without wait_arm')
-
+				self.set_action('arm')
 			elif received_command == self._COMMAND_LAND:
 				if self.current_action == 'wait_land':
 					self.pixhawk.commands.next += 1 # advance to the landing waypoint
 					self.current_action = 'landing'
 				else: self._log('WARNING - Cannot land while ' + self.current_action)
-
+			elif received_command == self._COMMAND_RTL:
+				self.set_action('start_rtl')
+			elif received_command == self._COMMAND_PAUSE:
+				self.set_action('pause')
 			elif received_command == self._COMMAND_SET_MISSION:
-				self.current_action = 'upload_new_mission'
+				if not self.pixhawk.armed:
+					wp_file = self.server.get_job()['wp_file']
+					wp_to_load = self.wp_path + '/' + wp_file + '.txt'
+					# TODO: figure out how to error check command upload
+					upload(self.pixhawk, self.wp_path + '/' + wp_file + '.txt')
+					self.pixhawk.commands.download()
+					self.current_action = 'idle'
+
+			elif recieved_command == self._COMMAND_SHUTDOWN:
+				if not self.pixhawk.armed:
+					self.stop()
+					# os.system("sudo shutdown -h now")
 
 			self._prev_command = received_command
 
-		# ACT ON ONGOING COMMAND
+
+		# ACT ON ONGOING ACTION
 		# the self.current_action allows us to have ongoing instructions
 		if self.current_action == 'prearm':
 			self.pixhawk.mode = VehicleMode('GUIDED') # we arm and takeoff in guided mode
 			self.pixhawk.commands.wait_ready() # we can't fly until we have commands list
+			self._arming_window_start = time.time()
 			self.current_action = 'wait_arm'
+
+		elif self.current_action == 'wait_arm' and time.time() - self._arming_window_start > 60:
+			self._log('TIMEOUT - revert to idle')
+			self.current_action = 'idle'
 
 		elif self.current_action == 'arm':
 			self.pixhawk.armed = True
 			self.current_action = 'takeoff'
-
-		elif self.current_action == 'disarm': 
-			self.pixhawk.armed = False
-			if not self.pixhawk.armed: self.current_action = ''
-			else:	self._log('DISARM FAILED')
 
 		elif self.current_action == 'takeoff' \
 			and self.pixhawk.armed: # we need to verify that the pixhawk is armed
@@ -140,30 +147,67 @@ class Drone:
 				self.current_action = 'wait_land'
 
 		elif self.current_action == 'landing' and not self.pixhawk.armed:
+			# we were landing and now we're disarmed, so we must have landed
 			self.gripper.open()
-			self.current_action = ''
+			self.current_action = 'idle'
+
+		elif self.current_action == 'disarm': 
+			self._log('disarm')
+			self.pixhawk.armed = False
+			if not self.pixhawk.armed: self.current_action = 'idle'
+			else:	self._log('DISARM FAILED')
 
 		elif self.current_action == 'pause':
 			self.pixhawk.mode = VehicleMode('LOITER')
 			self._log('LOITER')
-			self.current_action = ''
+			self.current_action = 'loiter'
 
-		elif self.current_action == 'rtl':
+		elif self.current_action == 'start_rtl':
 			self.pixhawk.mode = VehicleMode('RTL')
 			self._log('RTL')
-			self.current_action = ''
-
-		elif self.current_action == 'upload_new_mission':
-			if not self.pixhawk.armed:
-					wp_file = self.server.get_job()['wp_file']
-					wp_to_load = self.wp_path + '/' + wp_file + '.txt'
-					if upload_and_verify(self.pixhawk, self.wp_path + '/' + wp_file + '.txt'):
-						self.pixhawk.commands.download() # download the new commands
-						self.current_action = ''
-					else: 
-						self._log('Failed to upload the mission: ' + wp_to_load)
+			self.current_action = 'rtl'
 
 
+	#################################################################################
+	# STATE FLOW LOGIC
+	#################################################################################
+	def set_action(self, new_action):
+		if new_action == 'start_rtl':
+			# disarm if we haven't taken off yet
+			if self.current_action == 'prearm' \
+			or self.current_action == 'wait_arm' \
+			or self.current_action == 'arm' \
+			or self.current_action == 'takeoff' \
+			or self.current_action == 'disarm':
+				self.current_action = 'disarm'
+
+			else: self.current_action = 'start_rtl'
+
+		elif new_action == 'pause':
+			# disarm if we haven't taken off yet
+			if self.current_action == 'prearm' \
+			or self.current_action == 'wait_arm' \
+			or self.current_action == 'arm' \
+			or self.current_action == 'takeoff' \
+			or self.current_action == 'disarm':
+				self.current_action = 'disarm'
+
+			# pause if we are flying
+			elif self.current_action == 'flying' \
+			or self.current_action == 'mission_start' \
+			or self.current_action == 'landing' \
+			or self.current_action == 'rtl':
+				self.current_action = 'pause'
+
+			else: self._log('WARNING - Cannot pause while ' + self.current_action)
+
+		elif new_action == 'prearm':
+			if self.current_action == 'idle': self.current_action = 'prearm'
+			else: self._log('WARNING - Cannot start while ' + self.current_action)
+
+		elif new_action == 'arm':
+			if self.current_action == 'wait_arm': self.current_action = 'arm'
+			else: self._log('WARNING - Cannot arm and takeoff while ' + self.current_action)
 
 	#################################################################################
 	# UTIL
@@ -177,10 +221,6 @@ class Drone:
 			config_json['auth'], 
 			config_json['api_url'], 
 			config_json['name'])
-		# self.api_url = config_json['api_url']
-		# self.name = config_json['name']
-		# self.uid = config_json['uid']
-		# self.auth = config_json['auth']
 		self.status = config_json['startup_status']
 		self.wp_path = config_json['wp_path']
 		self._log('Successfully loaded config from ' + file)
@@ -206,8 +246,6 @@ class Drone:
 			"rssi": self.pixhawk.last_heartbeat # use the time since the last heartbeat becase we don't have internet connectivity
 		}
 		return self.state
-
-
 
 
 if __name__ == "__main__":
--- droneberry/missionHandler.py
+++ droneberry/missionHandler.py
@@ -21,31 +21,31 @@ def read(vehicle, missionFile):
 
     This function is used by upload_mission().
     """
-    print "\nReading mission from file: %s" % missionFile
     cmds = vehicle.commands
     missionlist=[]
-    with open(missionFile) as f:
-        for i, line in enumerate(f):
-            if i==0:
-                if not line.startswith('QGC WPL 110'):
-                    raise Exception('File is not supported WP version')
-            else:
-                linearray=line.split('\t')
-                ln_index=int(linearray[0])
-                ln_currentwp=int(linearray[1])
-                ln_frame=int(linearray[2])
-                ln_command=int(linearray[3])
-                ln_param1=float(linearray[4])
-                ln_param2=float(linearray[5])
-                ln_param3=float(linearray[6])
-                ln_param4=float(linearray[7])
-                ln_param5=float(linearray[8])
-                ln_param6=float(linearray[9])
-                ln_param7=float(linearray[10])
-                ln_autocontinue=int(linearray[11].strip())
-                cmd = Command( 0, 0, 0, ln_frame, ln_command, ln_currentwp, ln_autocontinue, 
-                    ln_param1, ln_param2, ln_param3, ln_param4, ln_param5, ln_param6, ln_param7)
-                missionlist.append(cmd)
+    f = open(missionFile)
+    for i, line in enumerate(f):
+        if i==0:
+            if not line.startswith('QGC WPL 110'):
+                raise Exception('File is not supported WP version')
+        else:
+            linearray=line.split('\t')
+            ln_index=int(linearray[0])
+            ln_currentwp=int(linearray[1])
+            ln_frame=int(linearray[2])
+            ln_command=int(linearray[3])
+            ln_param1=float(linearray[4])
+            ln_param2=float(linearray[5])
+            ln_param3=float(linearray[6])
+            ln_param4=float(linearray[7])
+            ln_param5=float(linearray[8])
+            ln_param6=float(linearray[9])
+            ln_param7=float(linearray[10])
+            ln_autocontinue=int(linearray[11].strip())
+            cmd = Command( 0, 0, 0, ln_frame, ln_command, ln_currentwp, ln_autocontinue, 
+                ln_param1, ln_param2, ln_param3, ln_param4, ln_param5, ln_param6, ln_param7)
+            missionlist.append(cmd)
+    f.close()
     return missionlist
 
 
@@ -54,18 +54,18 @@ def upload(vehicle, missionFile):
     Upload a mission from a file. 
     """
     #Read mission from file
+    print '[UPLOAD]',
     missionlist = read(vehicle, missionFile)
-    
-    print "\nUpload mission from a file: %s" % missionFile,
     #Clear existing mission from vehicle
-    print '\tClear mission',
+    print ' - read from', missionFile,
     cmds = vehicle.commands
     cmds.clear()
+    print ' - clear',
     #Add new mission to vehicle
     for command in missionlist:
         cmds.add(command)
-    print '\tUpload mission'
     vehicle.commands.upload()
+    print ' - upload'
     return missionlist
 
 
@@ -87,6 +87,8 @@ def download(vehicle):
 def upload_and_verify(vehicle, missionFile):
     uploaded_mission = upload(vehicle, missionFile)
     downloaded_mission = download(vehicle)
+    print uploaded_mission
+    print downloaded_mission
     return uploaded_mission == downloaded_mission
 
 
