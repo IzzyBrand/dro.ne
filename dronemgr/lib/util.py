584821dd0faf8265065666d8b9d04b99bdf74912
Izzy Brand <izzy@Izzys-MacBook-Pro.local>
2017-05-08 20:26:21 -0400

    Moved get and set in dronemgr to util

--- dronemgr/lib/util.py
+++ dronemgr/lib/util.py
@@ -71,3 +71,96 @@ class Status:
     def out(self,msg):
         t = time.strftime("%Y-%m-%d %H:%M:%S")
         print "[{}] {}".format(t,msg)
+
+
+""" Get information from database """
+class Get:
+
+    def __init__(self,db):
+        self.db = db
+
+    def position(self,uid):
+        return {
+            "latitude": self.db.get(uid,"latitude","drones"),
+            "longitude": self.db.get(uid,"longitude","drones"),
+            "altitude": self.db.get(uid,"altitude","drones"),
+            "speed": self.db.get(uid,"speed","drones"),
+            "timestamp": self.db.get(uid,"timestamp","drones")
+        }
+
+    def type(self,uid):
+        type_uid = self.db.get(uid,"type","drones")
+        return {
+            "maxpayload": self.db.get(type_uid,"maxpayload","types"),
+            "minvoltage": self.db.get(type_uid,"minvoltage","types"),
+            "topspeed": self.db.get(type_uid,"topspeed","types"),
+            "description": self.db.get(type_uid,"description","types")
+        }
+
+    def zone(self,uid):
+        zone_uid = self.db.get(uid,"zone","drones")
+        return {
+            "latitude": self.db.get(zone_uid,"latitude","zones"),
+            "longitude": self.db.get(zone_uid,"longitude","zones"),
+            "altitude": self.db.get(zone_uid,"altitude","zones"),
+            "description": self.db.get(zone_uid,"description","zones")
+        }
+
+    def job(self,uid):
+        job_uid = self.db.get(uid,"job","drones")
+        return {
+            "uid": self.db.get(job_uid,"uid","jobs"),
+            "username": self.db.get(job_uid,"username","jobs"),
+            "flavor": self.db.get(job_uid,"flavor","jobs"),
+            "destination": self.db.get(job_uid,"destination","jobs"),
+            "timestamp": self.db.get(job_uid,"timestamp","jobs")
+        }
+
+    def state(self,uid):
+        return {
+            "command": self.db.get(uid,"command","drones"),
+            "status": self.db.get(uid,"status","drones"),
+            "error": self.db.get(uid,"error","drones"),
+            "voltage": self.db.get(uid,"voltage","drones")
+        }
+
+    def general(self,uid):
+        return {
+            "name": self.db.get(uid,"name","drones"),
+            "description": self.db.get(uid,"description","drones")
+        }
+
+    def all(self,uid):
+        return {
+            "position": self.position(),
+            "type": self.type(),
+            "zone": self.zone(),
+            "state": self.state(),
+            "general": self.general()
+        }
+
+    def order(self,uid):
+        # TODO: this will need to be updated once we clarify exactly what the orders
+        # db structure is. do we have a field for eta? for which drone? for completed?
+        return {
+            'uid': uid,
+            'destination': self.db.get(uid, 'destination', 'orders'),
+            'timestamp': self.db.get(uid, 'timestamp', 'orders')
+            'drone_uid': self.db.get(uid, 'drone_uid', 'orders')
+        }
+
+
+""" Set information in database """
+class Set:
+
+    def __init__(self,db):
+        self.db = db
+
+    def status(self,uid,new_status):
+        self.db.set(new_status,uid,"status","drones")
+
+    def job(self,uid,new_job=None):
+        self.db.set(new_job,uid,"job","drones")
+
+    def command(self,uid, new_command):
+        self.db.set(new_command,uid,"command","drones")
